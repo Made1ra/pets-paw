0733b7c8c1072a6b49a6e4affc15b43589f97c2e
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-08-22 11:42:53 +0300

    Create Welcome.tsx

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+import voteTable from '../assets/vote-table.svg';
+import petBreeds from '../assets/pet-breeds.svg';
+import imagesSearch from '../assets/images-search.svg';
+import Rectangle from './Rectangle';
+import TextButton from './TextButton';
+
+const HelloParagraph = styled.p`
+    width: 11.375rem;
+
+    color: #1D1D1D;
+    font-family: Jost;
+    font-size: 2.75rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 3.625rem;
+`;
+
+const WelcomeParagraph = styled.p`
+    color: #8C8C8C;
+    font-family: Jost;
+    font-size: 1.25rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+`;
+
+const LetsStartParagraph = styled.p`
+    color: #1D1D1D;
+    font-family: Jost;
+    font-size: 1.25rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+`;
+
+const RectanglesContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+`;
+
+function Welcome() {
+    const rectangles = [
+        {
+            backgroundColor: '#B4B7FF',
+            url: voteTable,
+            text: 'VOTING'
+        },
+        {
+            backgroundColor: '#97EAB9',
+            url: petBreeds,
+            text: 'BREEDS'
+        },
+        {
+            backgroundColor: '#FFD280',
+            url: imagesSearch,
+            text: 'GALLERY'
+        }
+    ];
+
+    return (
+        <>
+            <HelloParagraph>Hi!👋</HelloParagraph>
+            <WelcomeParagraph>Welcome to MacPaw Bootcamp 2023</WelcomeParagraph>
+            <br />
+            <LetsStartParagraph>Lets start using The Cat API</LetsStartParagraph>
+            <RectanglesContainer>
+                {rectangles.map((rectangle) => (
+                    <div key={rectangle.text}>
+                        <Rectangle
+                            $backgroundColor={rectangle.backgroundColor}
+                            $url={rectangle.url}
+                        />
+                        <TextButton>{rectangle.text}</TextButton>
+                    </div>
+                ))}
+            </RectanglesContainer>
+        </>
+    );
+}
+
+export default Welcome;
