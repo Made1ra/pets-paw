3c6d0701a91fead0021351c8374abc4fea64f4e9
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-09-22 19:27:37 +0300

    fix: update pages

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,37 +1,17 @@
 import { useState } from 'react';
-import styled from 'styled-components';
 import Container from '../components/Container';
 import LeftContent from '../components/LeftSection';
 import RightSectionContainer from '../components/RightSectionContainer';
 import LinkContainer from '../components/LinkContainer';
-import Text from '../components/TextSpan';
+import TextSpan from '../components/TextSpan';
+import BoldText from '../components/BoldText';
 import SearchBar from '../components/SearchBar';
 import Smiles from '../components/Smiles';
 import ActionsContainer from '../components/ActionsContainer';
 import NavigationContainer from '../components/NavigationContainer';
 import SmallLink from '../components/SmallLink';
 import LargeTextButton from '../components/LargeTextButton';
-
-const Image = styled.div<{ $url: string }>`
-    width: 40rem;
-    height: 22.5rem;
-    flex-shrink: 0;
-
-    border-radius: 1.25rem;
-    background: url(${props => props.$url}), lightgray 50% no-repeat;
-    background-size: cover;
-
-    margin-top: 1rem;
-`;
-
-const BoldText = styled.span`
-    color: #1D1D1D;
-    font-family: Jost;
-    font-size: 1.25rem;
-    font-style: normal;
-    font-weight: 500;
-    line-height: normal;
-`;
+import Image from '../components/Image';
 
 function Search() {
     const API_KEY = import.meta.env.VITE_API_KEY;
@@ -63,15 +43,15 @@ function Search() {
                         <LargeTextButton>SEARCH</LargeTextButton>
                     </NavigationContainer>
                     {searchedBreeds.length > 0 && (
-                        <Text>
+                        <TextSpan>
                             Search results for: <BoldText>{searchedBreeds[0].name}</BoldText>
-                        </Text>
+                        </TextSpan>
                     )}
                     {
                         searchedBreeds.map((breed) => (
                             <Image
                                 key={breed.reference_image_id}
-                                $url={breed.image.url || ''}
+                                src={breed.image.url || ''}
                             />
                         ))
                     }
