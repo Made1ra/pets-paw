44b57d2df48f8fe14f25809674cfe0e39311d7fc
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-09-23 11:31:33 +0300

    fix: update SearchButton.tsx

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -6,8 +6,7 @@ function SearchButton({ onClick }: SearchButtonProps) {
     return (
         <div
             onClick={onClick}
-            className={`flex items-start w-10 h-10 mr-4 bg-red-100 rounded-[10px] bg-[url('src/assets/search-20.svg')] bg-no-repeat bg-center hover:bg-[url(src/assets/search-white-20.svg)] hover:bg-rose-400`}
-
+            className={`flex items-start w-10 h-10 mr-4 bg-red-100 rounded-[10px] bg-[url('../src/assets/search-20.svg')] bg-no-repeat bg-center hover:bg-[url(../src/assets/search-white-20.svg)] hover:bg-rose-400`}
         />
     );
 }
