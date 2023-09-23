8e0437d76a5fcff4fbbfbc42058eb25d10588000
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-09-23 12:17:28 +0300

    fix: update components

--- src/components/PreviousButton.tsx
+++ src/components/PreviousButton.tsx
@@ -1,39 +1,7 @@
-import styled from 'styled-components';
-import back from '../assets/back-20.svg';
-import backWhite from '../assets/back-white-20.svg';
-
-const StyledPreviousButton = styled.button`
-    display: flex;
-    padding: 0.75rem 1.875rem;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 0.625rem;
-
-    border-radius: 0.625rem;
-    border: none;
-    background: #FBE0DC;
-
-    &:hover {
-        background: #FF868E;
-    }
-`;
-
-const Back = styled.div`
-    width: 1rem;
-    height: 1rem;
-    background: url(${back}) center no-repeat;
-
-    &:hover {
-        background: url(${backWhite}) center no-repeat;
-    }
-`;
-
 function PreviousButton() {
     return (
-        <StyledPreviousButton>
-            <Back />
-        </StyledPreviousButton>
+        <div className="w-10 h-10 bg-red-100 rounded-[10px] bg-center bg-no-repeat bg-[url('..src/assets/back-20.svg')]
+        hover:bg-[url('../src/assets/back-white-20.svg')]" />
     );
 }
 
--- src/components/SmallLink.tsx
+++ src/components/SmallLink.tsx
@@ -7,7 +7,8 @@ function SmallLink() {
             style={{ textDecoration: 'none' }}
         >
             <div
-                className="flex items-center justify-center w-10 h-10 bg-red-100 rounded-[10px] bg-no-repeat bg-center bg-[url('src/assets/back-20.svg')] hover:bg-rose-400 hover:bg-[url('src/assets/back-white-20.svg')]"
+                className="flex items-center justify-center w-10 h-10 bg-red-100 rounded-[10px] bg-no-repeat bg-center bg-[url('../src/assets/back-20.svg')]
+                hover:bg-rose-400 hover:bg-[url('../src/assets/back-white-20.svg')]"
             />
         </Link >
     );
