0aaa9c463bb4162e937518dc39beb54277c28701
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-10-01 16:28:33 +0300

    feat: create loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+function Loader() {
+    return (
+        <div className="self-center w-[6.25rem] h-[6.25rem] rotate-45 rounded-[6.25rem] border-4 border-red-100" />
+    );
+}
+
+export default Loader;
