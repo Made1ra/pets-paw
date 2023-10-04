ebe03c15cafcd7825913c7b4275fc7795dbbf5a7
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-10-04 17:42:34 +0300

    fix: add alt attribute to Image

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,12 +1,14 @@
 type ImageProps = {
     src: string;
+    alt?: string;
 };
 
-function Image({ src }: ImageProps) {
+function Image({ src, alt }: ImageProps) {
     return (
         <img
             className="w-[640px] h-[360px] rounded-[20px]"
             src={src}
+            alt={alt}
         />
     );
 }
