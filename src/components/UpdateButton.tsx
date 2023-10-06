55dce2f4a3c88f1f985753161b166d4e95738c02
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-10-06 17:24:23 +0300

    fix: update UpdaateButton.tsx

--- src/components/UpdateButton.tsx
+++ src/components/UpdateButton.tsx
@@ -5,7 +5,7 @@ type UpdateButtonProps = {
 function UpdateButton({ onClick }: UpdateButtonProps) {
     return (
         <div
-            className="w-10 h-10 bg-white rounded-[10px] bg-no-repeat bg-center bg-[url('../src/assets/update-20.svg')] cursor-pointer
+            className="w-10 h-10 bg-white ml-4 rounded-[10px] bg-no-repeat bg-center bg-[url('../src/assets/update-20.svg')] cursor-pointer
             dark:bg-stone-900 hover:bg-rose-400 dark:hover:bg-rose-400 hover:bg-[url('../src/assets/update-white-20.svg')]"
             onClick={onClick}
         />
