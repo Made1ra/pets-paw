a347107332a4738aa0632910e23a576a089399f4
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-10-11 17:49:38 +0300

    fix: update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import UploadBackground from './UploadBackground';
+
 type ModalProps = {
     isOpen: boolean;
     onClose: () => void;
@@ -5,18 +8,35 @@ type ModalProps = {
 };
 
 function Modal({ isOpen, onClose, children }: ModalProps) {
+    const uploadImage = () => {
+
+    };
+
+    useEffect(() => {
+        document.body.style.overflow = 'hidden';
+
+        return () => {
+            document.body.style.overflow = 'auto'
+        };
+    }, []);
+
     if (!isOpen) {
         return null;
     }
 
     return (
-        <div className="flex flex-col w-[42.5rem] h-[52.5rem] bg-stone-50 rounded-[1.25rem] z-20">
+        <div className="flex flex-col w-[47.5rem] h-[57.5rem] bg-stone-50 rounded-[1.25rem] z-20 fixed top-5 right-24
+        dark:bg-stone-800">
             <button
-                className="self-end m-4 w-10 h-10 rounded-[10px] bg-center bg-no-repeat bg-[url('../src/assets/close-20.svg')]"
+                className="self-end m-4 w-10 h-10 rounded-[10px] bg-white bg-center bg-no-repeat bg-[url('../src/assets/close-20.svg')]
+                hover:bg-rose-400 hover:bg-[url('../src/assets/close-white-20.svg')]
+                dark:bg-opacity-5
+                dark:hover:bg-rose-400 dark:hover:bg-[url('../src/assets/close-white-20.svg')]"
                 onClick={() => onClose()}
             />
             <div className="flex flex-col text-center">
-                <div className="mt-8 text-stone-900 text-4xl font-medium font-jost">
+                <div className="mt-8 text-stone-900 text-4xl font-medium font-jost
+                dark:text-white">
                     Upload a .jpg or .png Cat Image
                 </div>
                 <span className="mt-4 text-neutral-400 text-xl font-normal font-jost leading-[1.875rem]">
@@ -30,9 +50,15 @@ function Modal({ isOpen, onClose, children }: ModalProps) {
                     </a>
                     or face deletion.
                 </span>
-                <div className="flex justify-center mt-8 w-[40rem] h-80 bg-white rounded-[1.25rem] border-2 border-red-100">
+                <div
+                    className="flex justify-center self-center mt-8 w-[40rem] h-80 bg-white rounded-[1.25rem] border-2 border-red-100 border-dashed
+                    dark:bg-opacity-5 dark:border-rose-400 dark:border-opacity-20"
+                    onClick={() => uploadImage()}
+                >
                     <div className="flex flex-col items-center justify-center">
-                        <div className="text-stone-900 text-xl font-medium font-jost leading-[1.875rem]">
+                        <UploadBackground />
+                        <div className="text-stone-900 text-xl font-medium font-jost leading-[1.875rem] z-20
+                        dark:text-white">
                             Drag here
                             <span className="text-neutral-400">
                                 {` your file or `}
