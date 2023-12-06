9a6c12cfac52b330d4d656189d727ce71eb9ad49
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-12-06 20:50:25 +0200

    fix: update controlsContainer.tsx

--- src/app/ui/controlsContainer.tsx
+++ src/app/ui/controlsContainer.tsx
@@ -4,7 +4,7 @@ export default function ControlsContainer({
     children?: React.ReactNode
 }) {
     return (
-        <div className="w-[11.625rem] h-[3.75rem] flex items-center justify-center bg-white rounded-2xl -mt-8 mb-4
+        <div className="w-[11.625rem] h-[3.75rem] flex items-center justify-center bg-white z-10 rounded-2xl -mt-8 mb-4
         sm:w-[16.5rem] sm:h-24 sm:-mt-12
         dark:bg-stone-50 dark:bg-opacity-10">
             {children}
